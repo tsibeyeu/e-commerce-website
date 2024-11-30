42fa7a441d3b81d427d905c6551e44269ae1a8b5
tsion beyene <tsibeyeu@gmail.com>
2024-12-01 01:36:48 +0300

    set theme

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,25 @@ import { BsCart3, BsMoonFill, BsSunFill } from "react-icons/bs";
 import { FaBarsStaggered } from "react-icons/fa6";
 import { NavLink } from "react-router-dom";
 import Navlinks from "./NavLinks";
+import { useEffect, useState } from "react";
+const themes = {
+  dim: "dim",
+  winter: "winter",
+};
+function getItemFromLocalStorage() {
+  return localStorage.getItem("theme") || themes.winter;
+}
 const Navbar = () => {
+  const [theme, setTheme] = useState(getItemFromLocalStorage());
+  function handleTheme() {
+    const { winter, dim } = themes;
+    const newTheme = theme === winter ? dim : winter;
+    setTheme(newTheme);
+  }
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme);
+  }, [theme]);
   return (
     <nav className="bg-base-200">
       <div className="align-element navbar">
@@ -18,19 +36,27 @@ const Navbar = () => {
               <FaBarsStaggered className="h-6 w-6" />
             </label>
             <ul className=" bg-base-200 menu menu-sm dropdown-content w-52 p-2 mt-3 rounded-box shadow z-[1]">
-              <Navlinks/>
+              <Navlinks />
             </ul>
           </div>
         </div>
         <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal"><Navlinks/></ul>
+          <ul className="menu menu-horizontal">
+            <Navlinks />
+          </ul>
         </div>
         <div className="navbar-end">
+          <label className="swap swap-rotate">
+            <input type="checkbox" onChange={handleTheme} />
+            <BsSunFill className="swap-on h-4 w-4" />
+            <BsMoonFill className="swap-off h-4 w-4" />
+          </label>
           <NavLink to="/cart" className="btn btn-circle btn-ghost ml-4 btn-md">
             <div className="indicator">
-                <BsCart3 className="h-6 w-6"/>
-                <span className="badge badge-primary badge-sm indicator-item">8</span>
-
+              <BsCart3 className="h-6 w-6" />
+              <span className="badge badge-primary badge-sm indicator-item">
+                8
+              </span>
             </div>
           </NavLink>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,13 @@
 /** @type {import('tailwindcss').Config} */
-import typography from '@tailwindcss/typography';  
-import daisyui from 'daisyui';  
+import typography from "@tailwindcss/typography";
+import daisyui from "daisyui";
 export default {
-  content: [
-    './src/**/*.{js,jsx,ts,tsx}',  
-    './index.html',  
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"],
   theme: {
     extend: {},
   },
-  plugins: [
-    typography,  
-    daisyui,  
-  ],
-}
-
+  plugins: [typography, daisyui],
+  daisyui: {
+    themes: ["dim", "winter"],
+  },
+};
