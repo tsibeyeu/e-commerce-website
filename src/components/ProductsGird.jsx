5cbf7d2bcc9e2f4d714c00e995e5fe3b62906b6a
tsion beyene <tsibeyeu@gmail.com>
2024-12-10 22:17:46 +0300

    some change made in component

--- src/components/ProductsGird.jsx
+++ src/components/ProductsGird.jsx
@@ -1,20 +1,28 @@
-import { Link, useLoaderData } from "react-router-dom";
-
-const ProductsGird = () => {
+import { Link, useLoaderData } from 'react-router-dom';
+const ProductsGrid = () => {
   const { products } = useLoaderData();
-  console.log(products);
+
   return (
-    <div>
+    <div className='pt-12 grid gap-4 md:grid-cols-2 lg:grid-cols-3 '>
       {products.map((product) => {
-        const { title, price, image } = product;
+        const { title, price, image } = product.attributes;
+        const dollarsAmount = price;
         return (
-          <Link key={product.id} to={`product/${product.id}`}>
-            <figure>
-              <img src={image} alt={title} />
+          <Link
+            key={product.id}
+            to={`/products/${product.id}`}
+            className='card w-full  shadow-xl hover:shadow-2xl transition duration-300 '
+          >
+            <figure className='px-4 pt-4'>
+              <img
+                src={image}
+                alt={title}
+                className='rounded-xl h-64 md:h-48 w-full object-cover'
+              />
             </figure>
-            <div>
-              <h2>{title}</h2>
-              <span>{price}</span>
+            <div className='card-body items-center text-center'>
+              <h2 className='card-title capitalize tracking-wider'>{title}</h2>
+              <span className='text-secondary'>{dollarsAmount}</span>
             </div>
           </Link>
         );
@@ -22,5 +30,4 @@ const ProductsGird = () => {
     </div>
   );
 };
-
-export default ProductsGird;
+export default ProductsGrid;
