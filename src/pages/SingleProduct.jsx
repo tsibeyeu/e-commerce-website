9877036503f39d36ac27a5e98f5cf9a809de7248
tsion beyene <tsibeyeu@gmail.com>
2024-12-17 00:17:28 +0300

    some change on single product

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -1,5 +1,5 @@
 import { Link, useLoaderData } from "react-router-dom";
-import { customFetch, formatPrice } from "../utils";
+import { customFetch, formatPrice, generateAmountOption } from "../utils";
 import { useState } from "react";
 export const loader = async ({ params }) => {
   const response = await customFetch(`/products/${params.id}`);
@@ -13,6 +13,10 @@ const SingleProduct = () => {
     product.attributes;
   const dollarsAmount = formatPrice(price);
   const [productColor, setProductColor] = useState(colors[0]);
+  const [amount, setAmount] = useState(1);
+  const handleAmount = (e) => {
+    setAmount(parseInt(e.target.value));
+  };
   // console.log(product.attributes);
   return (
     <section>
@@ -47,14 +51,42 @@ const SingleProduct = () => {
             </h4>
           </div>
           <div className="mt-2">
-            {colors.map(color=>{
-              return <button key={color} type="button" className={`badge  w-6 h-6 mr-2 ${productColor === color && 'border-2 border-secondary'}`} style={{backgroundColor:color}} onClick={() => setProductColor(color)}></button>
+            {colors.map((color) => {
+              return (
+                <button
+                  key={color}
+                  type="button"
+                  className={`badge  w-6 h-6 mr-2 ${
+                    productColor === color && "border-2 border-secondary"
+                  }`}
+                  style={{ backgroundColor: color }}
+                  onClick={() => setProductColor(color)}
+                ></button>
+              );
             })}
           </div>
-          
-          
-
-
+          <div className="form-control w-full max-w-xs">
+            <label className="label">
+              <h4 className="text-md font-medium tracking-wider capitalize">
+                amount
+              </h4>
+            </label>
+            <select
+              className="select select-secondary select-bordered select-md"
+              value={amount}
+              onChange={handleAmount}
+            >
+              {generateAmountOption(20)}
+            </select>
+          </div>
+          <div className="mt-3">
+            <button
+              className="btn btn-secondary btn-md"
+              onClick={() => console.log("added to bag")}
+            >
+              Add To Bag
+            </button>
+          </div>
         </div>
       </div>
     </section>
---
+++ src/utils/index.jsx
@@ -0,0 +1,23 @@
+import axios from "axios";
+const productionUrl = " https://strapi-store-server.onrender.com/api/";
+export const customFetch = axios.create({
+  baseURL: productionUrl,
+});
+
+export const formatPrice = (price) => {
+  const dollarAmount = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  }).format((price / 100).toFixed(2));
+  return dollarAmount;
+};
+export const generateAmountOption = (number) => {
+  return Array.from({ length: number }, (_, index) => {
+    const amount = index + 1;
+    return (
+      <option key={amount} value={amount}>
+        {amount}
+      </option>
+    );
+  });
+};
