38d787e813a7d7f95d6e23bfdc9c0ad9dc2d0230
tsion beyene <tsibeyeu@gmail.com>
2024-12-31 23:02:59 +0300

    add item redux done

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -8,12 +8,30 @@ const initialState = {
   tax:0,
   orderTotal: 0,
 };
+
+const getCartFromLocalStorage=()=>{
+    return JSON.parse(localStorage.getItem("cart")) || initialState;
+}
 const cartSlice =createSlice({
     name: "cart",
-    initialState,
+    initialState:getCartFromLocalStorage(),
     reducers:{
         addItem:(state,action)=>{
             console.log(action.payload);
+            const {product} =action.payload;
+            const item=state.cartItems.find((i)=>i.cartId === product.cartId);
+            if(item){
+                item.amount +=product.amount;
+
+            }else{
+                state.cartItems.push(product);
+            }
+            state.numItemsInCart +=product.amount;
+            state.cartTotal +=product.price * product.amount;
+            state.tax=0.1 * state.cartTotal;
+            state.orderTotal = state.cartTotal + state.tax + state.shipping;
+            localStorage.setItem("cart",JSON.stringify(state))
+            toast.success("Item added to cart");
         },
         removeItem:(state,action)=>{}
     },
