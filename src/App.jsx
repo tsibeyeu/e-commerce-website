8fe3f8369f96a22553e7460fc3bf04285750e9d3
tsion beyene <tsibeyeu@gmail.com>
2025-01-08 10:36:41 +0300

    action created in login

--- src/App.jsx
+++ src/App.jsx
@@ -16,10 +16,14 @@ import {
   
 } from "./pages";
 import { ErrorElement } from "./components";
+// Loaders
 import {loader as loadingLanding} from "./pages/Landing"
 import {loader as singleProdcutLoading} from "./pages/SingleProduct" ;
 import {loader as prodcutsLoading} from "./pages/Products" ;
+// Actions
 import {action as registerAction} from "./pages/Register";
+import {action as loginAction} from "./pages/Login";
+import store from "./store";
 const router = createBrowserRouter([
   {
     path: "/",
@@ -66,6 +70,8 @@ const router = createBrowserRouter([
     path: "/login",
     element: <Login />,
     errorElement: <Error />,
+    action: loginAction(store),
+
   },
   {
     path: "/register",
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,10 @@
 /* eslint-disable no-unused-vars */
 import { Form, Link } from "react-router-dom";
 import { FormInput, SubmitBtn } from "../components";
+export const action=(store)=> async ()=>{
+  console.log(store);
+  return null;
+}
 const Login = () => {
   return (
     <section className="h-screen grid place-items-center">
